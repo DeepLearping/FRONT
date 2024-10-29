e55c8f1a49b9ed921e5ca6e9ac7cf5aa4cdac6f8
DESKTOP-3ML7ILS\20108 <yjkl0516@gmail.com>
2024-10-29 18:08:49 +0900

    [Feat] - Front → 기본 틀 생성 (지동현)     - npm create-react-app 실행     - 기본 디렉토리 및 파일 배치     - .gitignore → 논의 후 추가할 파일 설정하기     - dependencies         - axios         - react-router-dom, react-redux         - redux(redux-actions,redux-logger,redux-persist,redux-thunk)         - jwt-decode ? (고려사항)     - redux로 state 관리하는 샘플 파일 추가(추후 설명 예정)

---
+++ src/Store.js
@@ -0,0 +1,24 @@
+import rootReducer from './modules';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+import { createStore } from 'redux';
+import { applyMiddleware } from 'redux';
+import {thunk} from 'redux-thunk';
+import logger from 'redux-logger';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['user', 'review']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(
+    persistedReducer,
+    composeWithDevTools(applyMiddleware(thunk, logger))
+);
+
+
+export default store;
