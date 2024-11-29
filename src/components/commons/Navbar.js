f00cf48bda4a8fa0dd10ff0496a3806e15180d51
DESKTOP-178DQCP\20107 <qwes5674@gmail.com>
2024-11-29 18:06:55 +0900

    [FEAT] Navbar 챗팅 최근 기록 기능 구현

--- src/components/commons/Navbar.js
+++ src/components/commons/Navbar.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import { NavLink } from "react-router-dom";
+import React, {useState, useEffect} from 'react';
+import { NavLink, useNavigate } from "react-router-dom";
 import '../../css/Navbar.css';
 import mypageIcon from '../../images/mypage.png';
 import ProfileModal from "../ProfileModal";
@@ -11,6 +11,29 @@ const Navbar = () => {
     const [isProfileModalOpen, setProfileModalOpen] = useState(false);
     const token = localStorage.getItem('token');
     const userInfo = useSelector(state => state.user.userInfo)
+    const [recentChats, setRecentChats] = useState([]);
+    const navigate = useNavigate();
+
+    // 챗팅 최근 기록 불러오기
+    useEffect(() => {
+        fetch('http://localhost:8080/api/v1/chatRoom/select')
+        .then(response => response.json())
+        .then(data => {
+
+            const chatRooms = data.results.chatRooms;
+           // chatrooms 형태 
+            console.log(chatRooms)
+
+           if (Array.isArray(chatRooms)) {
+            const sortedChats = chatRooms.sort((a, b) => new Date(a.last_modified) - new Date(b.last_modified));
+            const topThreeChats = sortedChats.slice(0, 3); // 최대 3개 지정
+            setRecentChats(topThreeChats); // 상태 업데이트
+        } else {
+            console.error("chatRooms가 배열이 아닙니다:", chatRooms);
+        }
+    })
+    .catch(error => console.error("최근 챗팅 기록을 불러 올 수 없습니다: ", error));
+    }, []);
 
     const openLoginModal = () => {
         setModalOpen(true);
@@ -40,6 +63,10 @@ const Navbar = () => {
         
     }
 
+    const handleRecentChatClick = (sessionId) => {
+        navigate(`/chat_room?sessionId=${sessionId}`);
+    }
+
     return(
         <div className = "nav-bar">
             <h2 className="title"> 캐릭터와의 대화</h2>
@@ -58,6 +85,19 @@ const Navbar = () => {
                     <NavLink to="/character-guiz">캐릭터 퀴즈</NavLink>
                 </li>
             </ul> 
+
+            {/* 최근 기록 */}
+            <div className = "recent-chats">
+                <h3> 최근 기록 </h3>
+                <ul>
+                    {recentChats.map((chat) => (
+                        <li key = {chat.session_id} onClick= {() =>
+                            handleRecentChatClick(chat.session_id)} style={{cursor:'pointer'}}>
+                                {chat.room_name} - {chat.description}
+                        </li>
+                    ))}
+                </ul>
+            </div>
 
             {/* 마이페이지 */}
             <div className='profile-container' onClick={handleProfileClick}>
