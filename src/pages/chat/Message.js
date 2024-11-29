71f9bbd84b6d2f053deffa405d1bcf1de3e50b9a
DESKTOP-VUTCS5R\20112 <asdaron44@gmail.com>
2024-11-29 14:33:31 +0900

    .

--- src/pages/chat/Message.js
+++ src/pages/chat/Message.js
@@ -21,6 +21,32 @@ const Message = ({ role, content }) => {
   
   const imageUrl =`http://localhost:8080/api/v1/character${character.profileImage}`;
   const charName = character ? character.charName : '';
+
+  // FastAPI 호출 함수(이득규)
+  const playAudio = async (text) => {
+    try {
+      const response = await fetch("http://localhost:8000/chat/stream_audio", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text }), // AI 응답 텍스트 전달
+      });
+
+      if (!response.ok) {
+        throw new Error("음성 생성 실패");
+      }
+
+      const audioBlob = await response.blob(); // 음성 데이터 가져오기
+      const audioUrl = URL.createObjectURL(audioBlob); // Blob을 URL로 변환
+
+      // 오디오 재생
+      const audio = new Audio(audioUrl);
+      audio.play();
+    } catch (error) {
+      console.error("오류 발생:", error);
+    }
+  };
   
     return (
       <div>
@@ -28,7 +54,11 @@ const Message = ({ role, content }) => {
           <div className="chat-charInfo-chatRoom">
             <img className='charaImg-message-chatRoom' src={imageUrl} alt="캐릭터 이미지" />
             <p>{charName}</p>
-            <img className='playButton-chatRoom' src={playbutton} alt="재생버튼"></img>
+            <img 
+            className='playButton-chatRoom' 
+            src={playbutton} 
+            alt="재생버튼" 
+            onClick={() => playAudio(content)}></img>
           </div>
         )}
         <div className={`message-chatRoom ${role}`}>
