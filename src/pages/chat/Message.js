163d990138c3d2161fc39fafaa13399d373142ed
DESKTOP-VUTCS5R\20112 <asdaron44@gmail.com>
2024-12-02 15:00:29 +0900

    .

--- src/pages/chat/Message.js
+++ src/pages/chat/Message.js
@@ -14,9 +14,28 @@ const Message = ({ role, content, msgImgUrl }) => {
   const imageUrl =`http://localhost:8080/api/v1/character${character.profileImage}`;
   const charName = character ? character.charName : '';
 
+
+  // // 이모지 제거 함수(이득규)
+  // const removeEmojis = (text) => {
+  //   const emojiPattern = /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F1E0}-\u{1F1FF}\u{02500}-\u{02BEF}\u{02702}-\u{027B0}\u{024C2}-\u{1F251}]/gu;
+  //   return text.replace(emojiPattern, '');
+  // };
+
+  // const cleanText = (text) => {
+  //   // 공백과 특수문자들 제거 (필요시 다른 조건 추가)
+  //   return text.replace(/[^가-힣a-zA-Z0-9가-힣\s]/g, '').trim();
+  // };
+
   // FastAPI 호출 함수(이득규)
   const playAudio = async (text) => {
     try {
+
+      // 이모지 제거
+      // const filteredText = cleanText(removeEmojis(text));
+      // console.log("Filtered Text: ", filteredText); //로그 찍어보기
+
+      
+      
       const response = await fetch(`http://localhost:8000/chat/stream_audio?text=${encodeURIComponent(text)}`, {
         method: "GET",
         headers: {
